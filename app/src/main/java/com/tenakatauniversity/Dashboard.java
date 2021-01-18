0f3f115cf1b8b30d8ca5084aa28ab962eef09758
ALEX <aleqikam@gmail.com>
2021-01-18 14:43:22 +0300

    Create the dashboard fragment

---
+++ app/src/main/java/com/tenakatauniversity/Dashboard.java
@@ -0,0 +1,25 @@
+package com.tenakatauniversity;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Dashboard extends Fragment {
+
+    private DashboardViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dashboard_fragment, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/tenakatauniversity/DashboardViewModel.java
@@ -0,0 +1,7 @@
+package com.tenakatauniversity;
+
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
