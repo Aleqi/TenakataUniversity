e16a354721676e76a8cef9ee82b73f15d99d9505
ALEX <aleqikam@gmail.com>
2021-01-18 16:26:04 +0300

    Create Apply fragment

---
+++ app/src/main/java/com/tenakatauniversity/apply/Apply.java
@@ -0,0 +1,25 @@
+package com.tenakatauniversity.apply;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tenakatauniversity.R;
+
+public class Apply extends Fragment {
+
+    private ApplyViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.apply_fragment, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/tenakatauniversity/apply/ApplyViewModel.java
@@ -0,0 +1,7 @@
+package com.tenakatauniversity.apply;
+
+import androidx.lifecycle.ViewModel;
+
+public class ApplyViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
