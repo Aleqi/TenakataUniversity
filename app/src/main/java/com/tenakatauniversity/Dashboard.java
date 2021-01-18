ab12f399ce4a90c64647582232e95be5c42f1ca8
ALEX <aleqikam@gmail.com>
2021-01-18 15:02:57 +0300

    Add "Navigation" library dependencies

--- app/src/main/java/com/tenakatauniversity/Dashboard.java
+++ app/src/main/java/com/tenakatauniversity/Dashboard.java
@@ -1,16 +1,15 @@
 package com.tenakatauniversity;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import androidx.lifecycle.ViewModelProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.tenakatauniversity.databinding.DashboardFragmentBinding;
 
 public class Dashboard extends Fragment {
 
@@ -19,7 +18,9 @@ public class Dashboard extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dashboard_fragment, container, false);
+        DashboardFragmentBinding binding = DashboardFragmentBinding.inflate(inflater, container, false);
+        mViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
+        return binding.getRoot();
     }
 
 }
--- app/src/main/java/com/tenakatauniversity/DashboardViewModel.java
+++ app/src/main/java/com/tenakatauniversity/DashboardViewModel.java
@@ -1,7 +1,19 @@
 package com.tenakatauniversity;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class DashboardViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<Boolean> _navigateToApplyFragmentLiveData = new MutableLiveData<>();
+
+    LiveData<Boolean> navigateToApplyFragmentLiveData;
+
+
+    public LiveData<Boolean> getNavigateToApplyFragmentLiveData() {
+        return _navigateToApplyFragmentLiveData;
+    }
+
+
+
 }
